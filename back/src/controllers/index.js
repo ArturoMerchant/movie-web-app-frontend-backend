5a891ec8d660dac5e58aeab27777a7065be6442c
admin <admin@MacBook-Air-de-CIRO.local>
2024-11-04 15:10:45 -0600

    created express server

---
+++ back/src/controllers/index.js
@@ -0,0 +1,8 @@
+const testController = (req, res) => {
+    res.status(200).send("Te amo Majo LoRI")
+    
+};
+
+module.exports = {
+    testController,
+}
---
+++ front/tests/prueba.test.js
@@ -0,0 +1,5 @@
+describe("una prueba de tests que pasa siempre" , () => {
+    it("este debe test pasar siempre", () => {
+        expect(true).toBe(true);
+    })
+})
