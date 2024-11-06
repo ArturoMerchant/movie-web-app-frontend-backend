00336856b05bf9ea1478da8f368952afcce27b87
admin <admin@MacBook-Air-de-CIRO.local>
2024-11-05 22:53:24 -0600

    changes in fetch functionality

--- back/src/services/index.js
+++ back/src/services/index.js
@@ -1,17 +1,14 @@
 const axios = require("axios");
+const Movie = require("../models/movie");
 
 const API_URL = "https://students-api.up.railway.app/movies";
 
 const movieService = {
 
     getMovies : async function fetchMovies() {
-        try {
-            const response = await axios.get(API_URL);
-          return response.data; // Devuelve los datos de las películas
-        } catch (error) {
-            console.error("Error al obtener datos de películas:", error);
-            throw error;
-        }
+        const movies = await Movie.find();
+        console.log(movies)
+        return movies;
     },
 
 };
